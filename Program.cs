4ae1b4e53c57f80a53381dc28cd9fea6d2f7dc9a
X-s0ft <d4vanchik@yandex.ru>
2023-03-06 14:02:35 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+
+Console.WriteLine("Hello, World!");
+int[,] masd = new int[5, 5];
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        if (i + j == 4)
+            masd[i, j] = i + 1;
+        Console.WriteLine(masd[i, j].ToString() + ' ');
+    }
+    Console.WriteLine();
+}
+
+//2
+double[,] masdd = new double[3, 4];
+Random random= new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        masdd[i, j] = Math.Round(random.NextDouble() * 100 - 50, 2);
+        Console.Write(masd[i, j].ToString() + ' ');
+    }
+    Console.WriteLine();
+    }
+
+
+
